edc1bb5f94a708c21fce57cb21eb005a69b9ac05
Leo Bergman <leo@hacksawstudios.com>
2018-04-04 17:54:53 +0200

    fix: Reduce idle CPU usage. chore: Make compatible with latest heaps

--- src/gasm/heaps/systems/HeapsCoreSystem.hx
+++ src/gasm/heaps/systems/HeapsCoreSystem.hx
@@ -26,23 +26,6 @@ class HeapsCoreSystem extends System implements ISystem {
             comp.init();
             comp.inited = true;
         }
-        switch(comp.componentType) {
-            case ComponentType.GraphicsModel: updateMouseCoords(comp);
-            default: null;
-        }
         comp.update(delta);
-    }
-
-    private function updateMouseCoords(comp:Component) {
-        var model:SpriteModelComponent = cast comp;
-        model.stageMouseX = root.mouseX;
-        model.stageMouseY = root.mouseY;
-        var spriteComponent:HeapsSpriteComponent = model.owner.get(HeapsSpriteComponent);
-        if (spriteComponent != null) {
-            var sp:Sprite = cast spriteComponent.sprite;
-            var p:h2d.col.Point  = sp.globalToLocal(new h2d.col.Point(root.mouseX, root.mouseY));
-            model.mouseX = p.x;
-            model.mouseY = p.y;
-        }
     }
 }
