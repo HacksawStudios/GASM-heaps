b41549ab3ffe68a4ae3ca9153cd6ffe51580d6ca
Sebastian Löfgren <sebastian@pandacreative.se>
2018-08-20 15:28:34 +0200

    Return the bitmap

--- src/gasm/heaps/text/ScalingTextField.hx
+++ src/gasm/heaps/text/ScalingTextField.hx
@@ -20,10 +20,12 @@ class ScalingTextField extends h2d.Text {
             actualW = calcTextWidth(text);
         }
     }
-    public inline function toBitmap(?marginX:Int = 0, ?marginY:Int = 0) {
+    public inline function toBitmap(?marginX:Int = 0, ?marginY:Int = 0):Bitmap {
         var tex = new Texture(Std.int(this.getSize().xMax) + marginX, Std.int(this.getSize().yMax) + marginY, [TextureFlags.Target]);
         this.drawTo(tex);
         var tile = Tile.fromTexture(tex);
         var bm = new Bitmap(tile);
+
+        return bm;
     }
 }
