38fb75b8b9f96009203cb567bf42a59a2bdd7fe4
Leo Bergman <leo@hacksawstudios.com>
2018-08-24 10:55:05 +0200

    feat: Make text component handle outlines and bitmap rendering

--- src/gasm/heaps/components/HeapsTextComponent.hx
+++ src/gasm/heaps/components/HeapsTextComponent.hx
@@ -2,7 +2,12 @@ package gasm.heaps.components;
 
 import gasm.core.Component;
 import gasm.heaps.text.ScalingTextField;
+import h2d.Tile;
+import h2d.Bitmap;
+import h2d.Sprite;
 import h2d.filter.Glow;
+import h3d.mat.Texture;
+import h3d.mat.Data.TextureFlags;
 import gasm.core.data.TextConfig;
 import h2d.Font;
 import h2d.Text;
@@ -14,7 +19,7 @@ import gasm.core.enums.ComponentType;
  * ...
  * @author Leo Bergman
  */
-class HeapsTextComponent extends Component {
+class HeapsTextComponent extends HeapsSpriteComponent {
 
     public var textField(default, null):ScalingTextField;
 
@@ -22,13 +27,18 @@ class HeapsTextComponent extends Component {
     var _font:h2d.Font;
     var _text:String;
     var _showOutline:Bool;
-    var _model:TextModelComponent;
+    var _textModel:TextModelComponent;
     var _lastW:Float;
     var _lastH:Float;
+    var _outlineMargin = 0.0;
+    var _holder:Sprite;
+    var _bitmap:Bitmap;
 
     public function new(config:TextConfig) {
+        super(new Sprite());
         _font = cast(config.font, h2d.Font);
-        textField = new ScalingTextField(_font);
+        _holder = new Sprite(sprite);
+        textField = new ScalingTextField(_font, _holder);
         var scale = config.size / _font.size;
         textField.scale(scale);
         textField.smooth = true;
@@ -40,47 +50,74 @@ class HeapsTextComponent extends Component {
     }
 
     override public function init() {
-        _model = owner.get(TextModelComponent);
-        _model.font = _config.font;
-        _model.size = _config.size;
-        _model.color = textField.textColor = _config.color;
+        super.init();
+        _textModel = owner.get(TextModelComponent);
+        _textModel.font = _config.font;
+        _textModel.size = _config.size;
+        _textModel.color = textField.textColor = _config.color;
         textField.textAlign = switch(_config.align) {
             case 'left': Align.Left;
             case 'right': Align.Right;
             default: Align.Center;
         };
-        textField.text = _model.text = _text;
+        textField.text = _textModel.text = _text;
         if(_config.filters != null) {
-           textField.filter = new h2d.filter.Group(cast _config.filters);
+           _holder.filter = new h2d.filter.Group(cast _config.filters);
         }
         
         textField.letterSpacing = _config.letterSpacing != null ? _config.letterSpacing : 0;
         var w = textField.getSize().width;
         var h = textField.getSize().height;
         if (w > 0) {
-            _model.width = w;
-            _model.height = h;
+            _textModel.width = w;
+            _textModel.height = h;
         }
         if(_config.scaleToFit){
             textField.scaleToFit(_config.width);
         }
+        if(_config.outlines != null && _config.outlines.length > 0) {
+            outline(cast _config.outlines);
+        }
+        if(_config.bitmap) {
+            toBitmap();
+        }
     }
 
-    public function outline(color:Int = 0x000000, alpha:Float = 1.0, radius:Float = 1.0, gain:Float = 1.0, quality:Float = 1.0) {
-        var glow = new Glow(color, alpha, radius, gain, quality, true);
-        textField.filter = glow;
+    function outline(outlines:Array<TextOutlineConfig>) {
+        var filters:Array<h2d.filter.Filter> = [];
+        for (outline in outlines) {
+            _outlineMargin += outline.radius;
+            filters.push(new Glow(outline.color, outline.alpha, outline.radius, outline.gain, outline.quality, true));
+        }
+        textField.x = textField.y = _outlineMargin;
+        textField.filter = new h2d.filter.Group(filters);
+    }
+
+    function toBitmap() {
+        if(_bitmap != null) {
+            _bitmap.remove();
+        }
+        _holder.visible = true;
+        var tex = new Texture(Std.int(sprite.getSize().xMax + _outlineMargin) , Std.int(sprite.getSize().yMax + _outlineMargin), [TextureFlags.Target]);
+        cast(_appModel.stage, h2d.Scene).addChild(_holder);
+        _holder.drawTo(tex);
+        cast(_appModel.stage, h2d.Scene).removeChild(_holder);
+        var tile = Tile.fromTexture(tex);
+        _bitmap = new Bitmap(tile);
+        _holder.visible = false;
+        sprite.addChild(bm);
     }
 
     override public function update(delta:Float) {
         var textChanged = false;
-        if (textField.text != _model.text) {
-            textField.text = _model.text;
-            textChanged = true; 
+        if (textField.text != _textModel.text) {
+            textField.text = _textModel.text;
+            textChanged = true;
         }
         var formatChanged = false;
-        if (_config.font != _model.font || _config.size != _model.size) {
-            _config.font = _model.font;
-            _config.size = _model.size;
+        if (_config.font != _textModel.font || _config.size != _textModel.size) {
+            _config.font = _textModel.font;
+            _config.size = _textModel.size;
             #if (heaps > "1.1.0")
             textField.font = Res.load(_config.font).to(hxd.res.Font).build(_config.size);
             #else
@@ -88,27 +125,37 @@ class HeapsTextComponent extends Component {
             #end
             formatChanged = true;
         }
-        if (_config.color != _model.color) {
-            textField.textColor = _config.color = _model.color;
+        if (_config.color != _textModel.color) {
+            textField.textColor = _config.color = _textModel.color;
         }
-        textField.x = _model.x + _model.offsetX;
-        textField.y = _model.y + _model.offsetY;
+        textField.x = _textModel.x + _textModel.offsetX;
+        textField.y = _textModel.y + _textModel.offsetY;
         var w = textField.getBounds().width;
         var h = textField.getBounds().height;
         if (w != _lastW) {
-            _model.width = w;
+            _textModel.width = w;
         }
         if (h != _lastH) {
-            _model.height = h;
+            _textModel.height = h;
         }
-        _lastW = _model.width;
-        _lastH = _model.height;
-        textField.visible = _model.visible;
+        _lastW = _textModel.width;
+        _lastH = _textModel.height;
+        textField.visible = _textModel.visible;
         if(textChanged || formatChanged) {
             if(_config.scaleToFit){
-                textField.scaleToFit(_model.width);
+                textField.scaleToFit(_textModel.width);
+            }
+            if(_config.bitmap) {
+                toBitmap();
             }
         }
     }
+}
 
+class TextOutlineConfig implements DataClass {
+    public var color:Int = 0x00000000;
+    public var alpha:Float = 1.0;
+    public var radius:Float = 1.0;
+    public var gain:Float = 1.0;
+    public var quality:Float = 1.0;
 }
--- src/gasm/heaps/text/ScalingTextField.hx
+++ src/gasm/heaps/text/ScalingTextField.hx
@@ -20,12 +20,4 @@ class ScalingTextField extends h2d.Text {
             actualW = calcTextWidth(text);
         }
     }
-    public inline function toBitmap(?marginX:Int = 0, ?marginY:Int = 0):Bitmap {
-        var tex = new Texture(Std.int(this.getSize().xMax) + marginX, Std.int(this.getSize().yMax) + marginY, [TextureFlags.Target]);
-        this.drawTo(tex);
-        var tile = Tile.fromTexture(tex);
-        var bm = new Bitmap(tile);
-
-        return bm;
-    }
 }
