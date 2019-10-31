4b22ded904111b61534742456fed020e07af41c3
Leo Bergman <leo@hacksawstudios.com>
2019-10-31 15:35:57 +0100

    fix: Use correct image path

--- src/gasm/heaps/HeapsContext.hx
+++ src/gasm/heaps/HeapsContext.hx
@@ -156,8 +156,9 @@ class HeapsContext extends App implements Context {
 		loader.addHandler(AssetType.Image, function(item:HandlerItem) {
 			final name = item.id;
 			final ext = Path.extension(item.path);
+			final imagePath = '$name.$ext';
 			asyncItems++;
-			getImageTexture(item, ext, name).then((texture) -> {
+			getImageTexture(imagePath, ext, name).then((texture) -> {
 				asyncItems--;
 				Reflect.setField(_assetContainers.images, item.id, Tile.fromTexture(texture));
 			});
