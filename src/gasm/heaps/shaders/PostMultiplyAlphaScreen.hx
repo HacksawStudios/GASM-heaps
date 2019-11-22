5ea12181c5bb87109f07afc5970240a0e45a407e
Leo Bergman <leo@hacksawstudios.com>
2019-11-22 15:02:30 +0100

    fix Avoid if

--- src/gasm/heaps/shaders/PostMultiplyAlphaScreen.hx
+++ src/gasm/heaps/shaders/PostMultiplyAlphaScreen.hx
@@ -5,10 +5,7 @@ class PostMultiplyAlphaScreen extends h3d.shader.ScreenShader {
 		@param var texture:Sampler2D;
 		function fragment() {
 			var color:Vec4 = texture.get(input.uv);
-
-			if (color.a > 0.0) {
-				color.rgb /= pow(color.a, 2.2);
-			}
+			color.rgb /= mix(1.0, pow(color.a, 2.2), ceil(color.a));
 			output.color = color;
 		}
 	}
