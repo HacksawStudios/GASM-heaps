69e18fa4019e4c33dc44385a289c5d7a70108880
Brinck <rasmus@hacksawstudios.com>
2020-03-31 15:54:24 +0200

    fix: restoring delay fixes in layout but with 2 standard frames delay

--- src/gasm/heaps/components/HeapsSceneBase.hx
+++ src/gasm/heaps/components/HeapsSceneBase.hx
@@ -5,8 +5,11 @@ import gasm.core.enums.ComponentType;
 
 class HeapsSceneBase extends Component {
 	public var scene:Any;
+	static public var sceneCounter = 0;
+	final _sceneId:Int;
 
 	public function new(scene:Any) {
+		_sceneId = sceneCounter++;
 		this.scene = scene;
 	}
 }
