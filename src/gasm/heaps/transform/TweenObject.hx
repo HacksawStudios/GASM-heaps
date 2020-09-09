8e4acd2347a20cba2053ed9ccf3628c970e51cf8
Leo Bergman <leo@hacksawstudios.com>
2020-09-09 09:58:07 +0200

    feat: Add TweenObject transform helper

---
+++ src/gasm/heaps/transform/TweenObject.hx
@@ -0,0 +1,488 @@
+package gasm.heaps.transform;
+
+import gasm.heaps.transform.TweenVector.TweenTarget;
+import gasm.heaps.transform.TweenVector.VectorTween;
+import h3d.Vector;
+import h3d.anim.Animation;
+import h3d.scene.Object;
+
+using Lambda;
+using Safety;
+using tink.CoreApi;
+
+enum ObjectTween {
+	Position(v:VectorTween);
+	Rotate(v:VectorTween);
+	Scale(v:VectorTween);
+	Color(v:VectorTween);
+}
+
+class TweenObjectBacking {
+	/**
+		3d object to wrap
+	**/
+	public var object:Object;
+
+	/**
+		Initial transform
+	**/
+	public var home:Transform;
+
+	public var parent(get, null):Object;
+	public var name(get, set):String;
+	public var visible(get, set):Bool;
+	public var x(get, set):Float;
+	public var y(get, set):Float;
+	public var z(get, set):Float;
+	public var scaleX(get, set):Float;
+	public var scaleY(get, set):Float;
+	public var scaleZ(get, set):Float;
+
+	@:allow(gasm.heaps.transform.TweenObject)
+	final position:TweenVector;
+	@:allow(gasm.heaps.transform.TweenObject)
+	final rotation:TweenVector;
+	@:allow(gasm.heaps.transform.TweenObject)
+	final scaling:TweenVector;
+	@:allow(gasm.heaps.transform.TweenObject)
+	final color:TweenVector;
+
+	/**
+		Private, only to be used by TweenObject.
+		To construct use implicit cast of TweenObject.
+		`final o:TweenObject = myObject;`
+	**/
+	@:allow(gasm.heaps.transform.TweenObject)
+	function new(object:Object) {
+		this.object = object;
+		final rot = object.getRotationQuat().toEuler();
+		position = new TweenVector(object.x, object.y, object.z);
+		rotation = new TweenVector(rot.x, rot.y, rot.z);
+		scaling = new TweenVector(object.scaleX, object.scaleY, object.scaleZ);
+		color = new TweenVector(1.0, 1.0, 1.0, 1.0);
+		home = {
+			pos: {x: position.x, y: position.y, z: position.z},
+			rotation: {x: rot.x, y: rot.y, z: rot.z},
+			scale: {x: object.scaleX, y: object.scaleY, z: object.scaleZ},
+			color: {
+				x: 1.0,
+				y: 1.0,
+				z: 1.0,
+				w: 1.0,
+			},
+		}
+	}
+
+	/**
+		Cancel all running tweens
+	**/
+	public function cancel() {
+		position.cancel();
+		rotation.cancel();
+		scaling.cancel();
+		color.cancel();
+	}
+
+	public function dispose() {
+		position.dispose();
+		rotation.dispose();
+		scaling.dispose();
+		color.dispose();
+	}
+
+	public function clone() {
+		final backing = new TweenObjectBacking(object.clone());
+		return backing;
+	}
+
+	/**
+		Return all materials in the underlying object.
+	**/
+	public function getMaterials() {
+		return object.getMaterials();
+	}
+
+	/**
+		Return all meshes in the underlying object.
+	**/
+	public function getMeshes() {
+		return object.getMeshes();
+	}
+
+	/**
+		Return child at index
+
+		@param index Index of child to get
+	**/
+	public function getChildAt(index:Int) {
+		return object.getChildAt(index);
+	}
+
+	/**
+		Return mesh by name in the underlying object.
+
+		@param name Name of mesh to get
+	**/
+	public function getMeshByName(name:String) {
+		return object.getMeshByName(name);
+	}
+
+	/**
+		Create an animation instance bound to the underlying object, set it as currentAnimation and play it.
+	**/
+	public function playAnimation(anim:Animation) {
+		object.playAnimation(anim);
+	}
+
+	/**
+		Return the quaternion representing the underlying object rotation.
+		Dot not modify as it's not a copy.
+	**/
+	public function getRotationQuat() {
+		return object.getRotationQuat();
+	}
+
+	public function setRotationQuat(q) {
+		object.setRotationQuat(q);
+	}
+
+	public function getCollider() {
+		return object.getCollider();
+	}
+
+	/**
+		Set the rotation of underlying object using the specified angles (in radian).
+	**/
+	public function setRotation(rx:Float, ry:Float, rz:Float) {
+		return object.setRotation(rx, ry, rz);
+	}
+
+	/**
+		Set the uniform scale for the underlying object.
+	**/
+	public function setScale(val:Float) {
+		return object.setScale(val);
+	}
+
+	/**
+		Add child
+	**/
+	public function addChild(obj:Object) {
+		return object.addChild(obj);
+	}
+
+	/**
+		Remove child
+	**/
+	public function removeChild(obj:Object) {
+		return object.removeChild(obj);
+	}
+
+	/**
+		Add child
+	**/
+	public function getBounds() {
+		return object.getBounds();
+	}
+
+	public function getScene() {
+		return object.getScene();
+	}
+
+	/**
+		Return an iterator over this object immediate children
+	**/
+	public inline function iterator():hxd.impl.ArrayIterator<Object> {
+		return new hxd.impl.ArrayIterator(@:privateAccess object.children);
+	}
+
+	/**
+		Set the position of the underlying object.
+	**/
+	public function setPosition(x:Float, y:Float, z:Float) {
+		position.set(x, y, z);
+		return object.setPosition(x, y, z);
+	}
+
+	/**
+		Reset and remove object.
+	**/
+	public function remove() {
+		cancel();
+		home = null;
+		object.remove();
+	}
+
+	function get_parent() {
+		return object.parent;
+	}
+
+	function get_name() {
+		return object.name;
+	}
+
+	function set_name(name:String) {
+		return object.name = name;
+	}
+
+	function get_visible() {
+		return object.visible;
+	}
+
+	function set_visible(val:Bool) {
+		return object.visible = val;
+	}
+
+	function get_x() {
+		return object.x;
+	}
+
+	function get_y() {
+		return object.y;
+	}
+
+	function get_z() {
+		return object.z;
+	}
+
+	function set_x(x:Float) {
+		position.x = x;
+		return object.x = x;
+	}
+
+	function set_y(y:Float) {
+		position.y = y;
+		return object.y = y;
+	}
+
+	function set_z(z:Float) {
+		position.z = z;
+		return object.z = z;
+	}
+
+	function get_scaleX() {
+		return object.scaleX;
+	}
+
+	function get_scaleY() {
+		return object.scaleY;
+	}
+
+	function get_scaleZ() {
+		return object.scaleZ;
+	}
+
+	function set_scaleX(val:Float) {
+		return object.scaleX = val;
+	}
+
+	function set_scaleY(val:Float) {
+		return object.scaleY = val;
+	}
+
+	function set_scaleZ(val:Float) {
+		return object.scaleZ = val;
+	}
+}
+
+@:forward
+abstract TweenObject(TweenObjectBacking) from TweenObjectBacking {
+	/**
+		Construct tween object
+
+		Private since not intended to be used directly, assign your h2d.scene.Object with implicit cast instead:
+		`final myObj:TweenObject = obj;`
+
+	**/
+	inline function new(obj:TweenObjectBacking, ?initial:Transform) {
+		this = obj;
+		init(initial);
+		if (initial != null) {
+			translateTo({to: initial.pos, duration: 0});
+			rotateTo({to: initial.rotation, duration: 0});
+			scaleTo({to: initial.scale, duration: 0});
+			colorTo({to: initial.color, duration: 0});
+		}
+	}
+
+	function init(?initial:Transform) {
+		if (initial == null) {
+			final posV = new Vector(this.x, this.y, this.z);
+			final rotV = this.getRotationQuat().toEuler();
+			final scaleV = new Vector(this.scaleX, this.scaleY, this.scaleZ);
+			final colorV = new Vector(1.0, 1.0, 1.0, 1.0);
+			initial = {
+				pos: posV,
+				rotation: rotV,
+				scale: scaleV,
+				color: colorV,
+			}
+		}
+
+		this.position.load(initial.pos);
+		this.rotation.load(initial.rotation);
+		this.scaling.load(initial.scale);
+		this.color.load(initial.color);
+		this.home = initial;
+	}
+
+	/**
+		Cancel running tweens and reset home
+	**/
+	public function resetTween() {
+		this.cancel();
+		init(this.home);
+	}
+
+	/**
+		Animate translation
+		@param tween Tween config for movement
+	**/
+	public function translateTo(tween:VectorTween):Future<Bool> {
+		return this.position.tween(tween);
+	}
+
+	/**
+		Animate rotation
+		@param tween Tween config for rotation
+	**/
+	public function rotateTo(tween:VectorTween):Future<Bool> {
+		this.rotation.load(this.getRotationQuat().toEuler());
+		return this.rotation.tween(tween);
+	}
+
+	/**
+		Animate scaling
+		@param tween Tween config for scaling
+	**/
+	public function scaleTo(tween:VectorTween):Future<Bool> {
+		return this.scaling.tween(tween);
+	}
+
+	/**
+		Animate color
+		@param tween Tween config for color
+	**/
+	public function colorTo(tween:VectorTween):Future<Bool> {
+		return this.color.tween(tween);
+	}
+
+	/**
+		Tween object
+		@param tweens Array of ObjectTweens to apply to object
+	**/
+	public function tween(tweens:Array<ObjectTween>):Future<Bool> {
+		return Future.async(done -> {
+			final allTweens = [
+				for (tween in tweens) {
+					switch tween {
+						case Position(v):
+							translateTo(v);
+						case Rotate(v):
+							rotateTo(v);
+						case Scale(v):
+							scaleTo(v);
+						case Color(v):
+							colorTo(v);
+					}
+				}
+			];
+			// Update to write the current value
+			update(0.0);
+			Future.ofMany(allTweens).handle(doneArray -> {
+				done(!doneArray.has(false));
+			});
+		});
+	}
+
+	/**
+		Return to initial position.
+
+		@param duration Transition time in seconds
+		@param curve Easing curve to use
+
+		@return Future resolved when transition is complete
+	**/
+	public function returnHome(duration = 0.0, ?curve:Float->Float):Future<Bool> {
+		curve = curve != null ? curve : f -> f;
+		return tween([
+			Position({to: this.home.pos, duration: duration, curve: curve}),
+			Rotate({to: this.home.rotation, duration: duration, curve: curve}),
+			Scale({to: this.home.scale, duration: duration, curve: curve}),
+			Color({to: this.home.color, duration: duration, curve: curve}),
+		]);
+	}
+
+	/**
+		Update animating values
+		@param dt Delta time
+	**/
+	public function update(dt:Float) {
+		if (this.position.update(dt)) {
+			this.x = this.position.x;
+			this.y = this.position.y;
+			this.z = this.position.z;
+		} else {
+			this.position.set(this.x, this.y, this.z);
+		}
+
+		if (this.rotation.update(dt)) {
+			this.setRotation(this.rotation.x, this.rotation.y, this.rotation.z);
+		}
+
+		if (this.scaling.update(dt)) {
+			this.scaleX = this.scaling.x;
+			this.scaleY = this.scaling.y;
+			this.scaleZ = this.scaling.z;
+		} else {
+			this.scaling.set(this.scaleX, this.scaleY, this.scaleZ);
+		}
+
+		if (this.color.update(dt)) {
+			for (material in this.getMaterials()) {
+				material.color.x = this.color.x;
+				material.color.y = this.color.y;
+				material.color.z = this.color.z;
+				material.color.w = this.color.w;
+			}
+		}
+	}
+
+	@:from
+	inline public static function fromBacking(obj:TweenObjectBacking) {
+		return new TweenObject(obj);
+	}
+
+	@:from
+	inline public static function fromObject(obj:Object) {
+		return new TweenObject(new TweenObjectBacking(obj));
+	}
+
+	@:to
+	inline public function toObject():Object {
+		return this.object;
+	}
+}
+
+@:structInit
+class Transform {
+	/**
+		Position target
+	**/
+	public var pos:TweenTarget = null;
+
+	/**
+		Scale target
+	**/
+	public var scale:TweenTarget = null;
+
+	/**
+		Rotation target
+	**/
+	public var rotation:TweenTarget = null;
+
+	/**
+		Color target
+	**/
+	public var color:TweenTarget = null;
+}
---
+++ src/gasm/heaps/transform/TweenVector.hx
@@ -0,0 +1,405 @@
+package gasm.heaps.transform;
+
+import gasm.core.utils.Assert;
+import h3d.Vector;
+
+using Lambda;
+using Safety;
+using tink.CoreApi;
+using tweenxcore.Tools;
+
+@:structInit
+class VectorTween {
+	/**
+		Target to tween to
+		Can be assigned from vector or from dynamic object specifying the axis to transform
+		So if you wish to only transform z axis 20 units, assign `{z:20}`
+	**/
+	public var to:TweenTarget = null;
+
+	/**
+		Target to tween from
+		Optional, and if omitted it will use current object transform
+		Can be assigned from vector or from dynamic object specifying the axis to transform
+	**/
+	public var from:TweenTarget = null;
+
+	/**
+		Called when tween is complete
+		@param done True when tween was completed 100%. False when tween was cancelled or replaced
+	**/
+	public var onDone:(done:Bool) -> Void = null;
+
+	/**
+		Delay start of tween in seconds
+	**/
+	public var delay = 0.0;
+
+	/**
+		Duration of tween in seconds
+	**/
+	public var duration = 1.0;
+
+	/**
+		Current elapsed time of running tween, or 0 if not running
+	**/
+	public var time = 0.0;
+
+	/**
+		Easing curve to use for tween
+	**/
+	public var curve:(x:Float) -> Float = f -> f;
+
+	/**
+		If true, to and from values will be in relation to objects current transform instead of absolute
+	**/
+	public var relative = false;
+
+	/**
+		For how many seconds of tween curve should be applied.
+	**/
+	public var curveDuration:Null<Float> = null;
+
+	/**
+		If true, tween will repeat until manually stopped
+	**/
+	public var repeat = false;
+}
+
+/**
+	3d space position with all values optional so one can describe change in only one axis.
+	Construct by assigning.
+	```
+	final pos:TweenPos = {x:10};
+	```
+**/
+@:structInit
+@:allow(TweenTarget)
+class TweenPos {
+	public var x:Float = null;
+	public var y:Float = null;
+	public var z:Float = null;
+	public var w:Float = null;
+}
+
+/**
+	Abstract over TweenPos, providing implicit casting to and from Vector
+**/
+@:forward
+abstract TweenTarget(TweenPos) from TweenPos to TweenPos {
+	public inline function new(val:TweenPos) {
+		this = val;
+	}
+
+	public function clone():TweenTarget {
+		return {
+			x: this.x,
+			y: this.y,
+			z: this.z,
+			w: this.w,
+		};
+	}
+
+	/**
+
+	**/
+	public function set(tp:TweenTarget, overwriteNulls = false) {
+		this.x = (this.x != null || overwriteNulls) ? tp.x : null;
+		this.y = (this.y != null || overwriteNulls) ? tp.y : null;
+		this.z = (this.z != null || overwriteNulls) ? tp.z : null;
+		this.w = (this.w != null || overwriteNulls) ? tp.w : null;
+	}
+
+	public function add(tp:TweenTarget) {
+		this.x = (this.x != null) ? this.x + tp.x : null;
+		this.y = (this.y != null) ? this.y + tp.y : null;
+		this.z = (this.z != null) ? this.z + tp.z : null;
+		this.w = (this.w != null) ? this.w + tp.w : null;
+	}
+
+	@:from
+	public static function fromVector(v:Vector) {
+		return new TweenTarget({
+			x: v.x,
+			y: v.y,
+			z: v.z,
+			w: v.w,
+		});
+	}
+
+	@:to
+	public function toVector():Vector {
+		if (this == null) {
+			return null;
+		}
+		return new Vector(this.x.or(0.0), this.y.or(0.0), this.z.or(0.0), this.w.or(1.0));
+	}
+}
+
+/**
+	Holds vector data adding tweening functionality
+**/
+@:forward
+abstract TweenVector(TweenVectorBacking) from TweenVectorBacking to TweenVectorBacking {
+	public inline function new(x:Float, y:Float, z:Float, w:Float = 1.0) {
+		this = new TweenVectorBacking(x, y, z, w);
+	}
+
+	/**
+		Tween values to target
+		@param target Tweening config
+	**/
+	public function tween(target:VectorTween):Future<Bool> {
+		return Future.async(done -> {
+			// Find _activeTweens already animating the target properties
+			if (this._activeTweens == null) {
+				this._activeTweens = [];
+			}
+
+			final hasFrom = target.from != null;
+			final hasTo = target.to != null;
+
+			Assert.that(target.to != null || target.from != null, 'Must supply to or from to tween');
+
+			// Ensure all input values have a valid start and endpoint
+			// If no target is specified. Tween to position at activation time
+			if (!hasTo) {
+				// Clone target from to preserve nulls
+				target.to = target.from.clone();
+				target.to.set(this);
+			}
+
+			// If only target is specified. Tween from origin
+			if (!hasFrom) {
+				// Clone target from to preserve nulls
+				target.from = target.to.clone();
+				target.from.set(this);
+			}
+
+			// Add relative coords if needed
+			if (target.relative) {
+				if (hasTo) {
+					target.to.add(this);
+				}
+				if (hasFrom) {
+					target.from.add(this);
+				}
+			}
+
+			// Replace on going tweens
+			cancelUsedAxis(target);
+
+			target.time = 0.0;
+			// Add animation
+			this._activeTweens.push(target);
+			target.onDone = (tweenDone:Bool) -> {
+				this._activeTweens.remove(target);
+				done(tweenDone);
+			}
+		});
+	}
+
+	public function cancelUsedAxis(t:VectorTween) {
+		if (this._activeTweens != null) {
+			var remove = [];
+			for (tween in this._activeTweens) {
+				// Replace axis if input not null
+				tween.to.x = t.to.x == null ? tween.to.x : null;
+				tween.to.y = t.to.y == null ? tween.to.y : null;
+				tween.to.z = t.to.z == null ? tween.to.z : null;
+				tween.to.w = t.to.w == null ? tween.to.w : null;
+
+				// All parts gone? Remove tween
+				if (tween.to.x == null && tween.to.y == null && tween.to.z == null && tween.to.w == null) {
+					remove.push(tween);
+				}
+			}
+			for (tween in remove) {
+				tween.onDone(false);
+				this._activeTweens.remove(tween);
+			}
+		}
+	}
+
+	inline public function set(x:Float, y:Float, z:Float, w:Float = null) {
+		this.x = x.or(this.x);
+		this.y = y.or(this.y);
+		this.z = z.or(this.z);
+		this.w = w.or(this.w);
+	}
+
+	inline public function scale(s:Float) {
+		this.x *= s;
+		this.y *= s;
+		this.z *= s;
+		this.w *= s;
+	}
+
+	inline public function reset() {
+		cancel();
+		this._activeTweens = [];
+	}
+
+	public function cancel() {
+		if (this._activeTweens != null) {
+			for (tween in this._activeTweens) {
+				cancelTween(tween);
+			}
+		}
+	}
+
+	/**
+		Stop repetition of all active tweens, results in onDone when current repetition is complete
+	**/
+	public function stopRepeat() {
+		if (this._activeTweens != null) {
+			for (tween in this._activeTweens) {
+				tween.repeat = false;
+			}
+		}
+	}
+
+	public function clone() {
+		return new TweenVector(this.x, this.y, this.z, this.w);
+	}
+
+	function cancelTween(tween:VectorTween) {
+		tween.to.x = null;
+		tween.to.y = null;
+		tween.to.z = null;
+		tween.to.w = null;
+		tween.onDone(false);
+	}
+
+	/**
+		Update current animating vector
+		@param dt Delta time
+	**/
+	public function update(dt:Float) {
+		final tweening = this._activeTweens != null && this._activeTweens.length > 0;
+		if (!tweening) {
+			return false;
+		}
+
+		for (tween in this._activeTweens) {
+			tween.time += dt;
+			if (tween.time < tween.delay) {
+				continue;
+			}
+			var done = false;
+			var p = (tween.time - tween.delay) / tween.duration;
+			if (p >= 1.0) {
+				done = tween.repeat ? false : true;
+				tween.time = tween.repeat ? 0.0 : tween.time;
+				p = 1.0;
+			}
+			final curveDuration = tween.curveDuration.or(tween.duration);
+			final curveStart = tween.duration - Math.min(tween.curveDuration, tween.duration);
+			final curvePart = curveStart / tween.duration;
+
+			// Animate X
+			if (tween.to.x != null) {
+				if (tween.curveDuration != null) {
+					final curveStart = tween.to.x - ((tween.to.x - tween.from.x) * curvePart);
+					if (p < 1 - curvePart) {
+						this.x = p.lerp(tween.from.x, curveStart);
+					} else {
+						this.x = tween.curve(p).lerp(curveStart, tween.to.x);
+					}
+				} else {
+					this.x = tween.curve(p).lerp(tween.from.x, tween.to.x);
+				}
+			}
+
+			// Animate Y
+			if (tween.to.y != null) {
+				if (tween.curveDuration != null) {
+					final curveStart = tween.to.y - ((tween.to.y - tween.from.y) * curvePart);
+					if (p < 1 - curvePart) {
+						this.y = p.lerp(tween.from.y, curveStart);
+					} else {
+						this.y = tween.curve(p).lerp(curveStart, tween.to.y);
+					}
+				} else {
+					this.y = tween.curve(p).lerp(tween.from.y, tween.to.y);
+				}
+			}
+
+			// Animate Z
+			if (tween.to.z != null) {
+				if (tween.curveDuration != null) {
+					final curveStart = tween.to.z - ((tween.to.z - tween.from.z) * curvePart);
+					if (p < 1 - curvePart) {
+						this.z = p.lerp(tween.from.z, curveStart);
+					} else {
+						this.z = tween.curve(p).lerp(curveStart, tween.to.z);
+					}
+				} else {
+					this.z = tween.curve(p).lerp(tween.from.z, tween.to.z);
+				}
+			}
+
+			// Animate W
+			if (tween.to.w != null) {
+				if (tween.curveDuration != null) {
+					final curveStart = tween.to.w - ((tween.to.w - tween.from.w) * curvePart);
+					if (p < 1 - curvePart) {
+						this.w = p.lerp(tween.from.w, curveStart);
+					} else {
+						this.w = tween.curve(p).lerp(curveStart, tween.to.w);
+					}
+				} else {
+					this.w = tween.curve(p).lerp(tween.from.w, tween.to.w);
+				}
+			}
+
+			if (done) {
+				tween.onDone(true);
+				continue;
+			}
+		}
+		return true;
+	}
+
+	@:from
+	inline public static function fromTweenPos(v:TweenPos):TweenVector {
+		return new TweenVector(v.x, v.y, v.z, v.w);
+	}
+
+	@:from
+	inline public static function fromVector(v:Vector):TweenVector {
+		return new TweenVector(v.x, v.y, v.z, v.w);
+	}
+
+	@:to
+	inline public function toVector():Vector {
+		return cast this;
+	}
+}
+
+/**
+	Backing class for TweenVector to hold active tweens
+**/
+class TweenVectorBacking extends Vector {
+	/**
+		Returns true if an animation is active
+	**/
+	public var isTweening(get, never):Bool;
+
+	// All active animations
+	@:allow(gasm.heaps.transform.TweenVector)
+	var _activeTweens:Array<VectorTween>;
+
+	public function new(x = 0.0, y = 0.0, z = 0.0, w = 1.0) {
+		super(x, y, z, w);
+		_activeTweens = [];
+	}
+
+	public function dispose() {
+		_activeTweens = null;
+	}
+
+	function get_isTweening():Bool {
+		return _activeTweens != null && _activeTweens.length > 0;
+	}
+}
