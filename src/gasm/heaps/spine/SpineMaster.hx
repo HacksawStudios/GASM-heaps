c2aec38c792300a355d4bd0dc5be37aab6d4a7e5
Sebastian Löfgren <60238220+nanowallets-guide@users.noreply.github.com>
2020-11-12 11:11:18 +0100

    chore: Organized imports

--- src/gasm/heaps/spine/SpineMaster.hx
+++ src/gasm/heaps/spine/SpineMaster.hx
@@ -1,53 +1,54 @@
 package gasm.heaps.spine;
 
 import gasm.heaps.data.Spine;
-import spine.SkeletonData;
-import spine.SkeletonJson;
-import spine.Skeleton;
-import spine.attachments.AtlasAttachmentLoader;
-import spine.support.graphics.TextureAtlas;
 import spine.AnimationState;
 import spine.AnimationStateData;
+import spine.Skeleton;
+import spine.SkeletonData;
+import spine.SkeletonJson;
+import spine.attachments.AtlasAttachmentLoader;
+import spine.support.graphics.TextureAtlas;
 
+// TODO: Naming?
 class SpineMaster {
-    public final skeleton:Skeleton;
-    public final animationState:AnimationState;
-    
-    final _skeletonData:SkeletonData;
-    final _loader:SpineTextureLoader;
-    final _textureAtlas:TextureAtlas;
-    final _atlasAttachmentLoader:AtlasAttachmentLoader;
-    final _skeletonJson:SkeletonJson;
-    final _spineConfigHandle:SpineConfigHandle;
-    final _animationStateData:AnimationStateData;
+	public final skeleton:Skeleton;
+	public final animationState:AnimationState;
 
-    public function new(spine:Spine) {
-        _loader = new SpineTextureLoader(spine.tile);
+	final _skeletonData:SkeletonData;
+	final _loader:SpineTextureLoader;
+	final _textureAtlas:TextureAtlas;
+	final _atlasAttachmentLoader:AtlasAttachmentLoader;
+	final _skeletonJson:SkeletonJson;
+	final _spineConfigHandle:SpineConfigHandle;
+	final _animationStateData:AnimationStateData;
+
+	public function new(spine:Spine) {
+		_loader = new SpineTextureLoader(spine.tile);
 		_textureAtlas = new TextureAtlas(spine.atlas, _loader);
-        _atlasAttachmentLoader = new AtlasAttachmentLoader(_textureAtlas);
+		_atlasAttachmentLoader = new AtlasAttachmentLoader(_textureAtlas);
 		_skeletonJson = new SkeletonJson(_atlasAttachmentLoader);
 		_spineConfigHandle = new SpineConfigHandle(spine.config);
-        _skeletonData = _skeletonJson.readSkeletonData(_spineConfigHandle);
-        _animationStateData = new AnimationStateData(_skeletonData);
-        animationState = new AnimationState(_animationStateData);
-        skeleton = new Skeleton(_skeletonData);
-        skeleton.updateWorldTransform();
-    }
+		_skeletonData = _skeletonJson.readSkeletonData(_spineConfigHandle);
+		_animationStateData = new AnimationStateData(_skeletonData);
+		animationState = new AnimationState(_animationStateData);
+		skeleton = new Skeleton(_skeletonData);
+		skeleton.updateWorldTransform();
+	}
 
-    public function dispose() {
+	public function dispose() {
 		if (_textureAtlas != null) {
 			_textureAtlas.dispose();
 		}
 	}
-    
-    /**
+
+	/**
 		Update spine elements
 		@param dt Delta time
 	**/
 	public function update(dt:Float) {
-        animationState.update(dt);
-        animationState.apply(skeleton);
+		animationState.update(dt);
+		animationState.apply(skeleton);
 		skeleton.updateWorldTransform();
 		skeleton.update(dt);
-    }
+	}
 }
