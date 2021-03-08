e90866d86333dc82d66d41eef766134ba9e64eb9
Sebastian Löfgren <sebastian@hacksawstudios.com>
2021-03-08 15:52:57 +0100

    fix: Added back bounds for Heaps3DViewportComponent

--- src/gasm/heaps/components/Heaps3DViewportComponent.hx
+++ src/gasm/heaps/components/Heaps3DViewportComponent.hx
@@ -3,6 +3,7 @@ package gasm.heaps.components;
 import gasm.core.Component;
 import gasm.core.components.AppModelComponent;
 import gasm.core.enums.ComponentType;
+import gasm.core.utils.Assert;
 import gasm.heaps.components.HeapsScene3DComponent;
 import h3d.Vector;
 
@@ -40,8 +41,10 @@ class Heaps3DViewportComponent extends Component {
 		cam.zNear = _config.zNear;
 		cam.zFar = _config.zFar;
 
-		fovRatio = _config.fovRatio;
-		fov = _config.fov;
+		if (_config.fov != null) {
+			fov = _config.fov;
+			fovRatio = _config.fovRatio;
+		}
 	}
 
 	function set_fov(val:Float) {
@@ -51,19 +54,51 @@ class Heaps3DViewportComponent extends Component {
 	}
 
 	function set_fovRatio(val:Float) {
+		Assert.that(fov != null, 'Trying to set fovRatio without a set fov');
 		fovRatio = val;
 		fov = fov;
 		return val;
+	}
+
+	override public function update(dt:Float) {
+		if (!_hasBounds && _config.boundsObject != null) {
+			var bounds = _config.boundsObject.getBounds();
+			// Empty bounds has values between -1e20 and 1e20
+			if (bounds.xMin != 1e20) {
+				_s3d.visible = true;
+				var top = bounds.ySize;
+				var right = bounds.xSize;
+				var w = _config.bounds2d.width / _appModel.pixelRatio;
+				var h = _config.bounds2d.height / _appModel.pixelRatio;
+				var wFactor = top / w;
+				var hFactor = right / h;
+				var wRatio = w * wFactor * _config.boundsMult.x;
+				var hRatio = h * hFactor * _config.boundsMult.y;
+				var ratio = Math.min(wRatio, hRatio);
+				if (_scale != ratio) {
+					_s3d.setScale(ratio);
+					_s3d.camera.update();
+					_scale = ratio;
+					if (fov != null) {
+						fovRatio = ratio;
+					}
+				}
+				_hasBounds = true;
+			}
+		}
 	}
 }
 
 @:structInit
 class Heaps3DViewportConfig {
+	public var boundsObject:h3d.scene.Object = null;
+	public var boundsMult = new Vector(1, 1);
+	public var bounds2d:h2d.col.Bounds = null;
 	public var cameraPos = new Vector(2, 3, 4);
 	public var cameraTarget = new Vector(-0.00001);
 	public var zNear = 1.0;
 	public var zFar = 100.0;
-	public var fov:Float = 45;
-	public var fovRatio:Float = 16 / 9;
+	public var fov:Null<Float> = null;
+	public var fovRatio:Float = 4 / 3;
 	public var rightHanded = true;
 }
